83b99104cfcc4bc942ca1f527bfcb8c40738f8ad
ttthuan <ttthuan001@gmail.com>
2017-12-24 20:52:20 +0700

    add 3 layer

--- SourceCode/QLKS/Program.cs
+++ SourceCode/QLKS/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace QLKS
+namespace PresentationLayer
 {
     static class Program
     {
@@ -15,7 +15,7 @@ namespace QLKS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SoDoKhachSan());
         }
     }
 }
---
+++ SourceCode/QLKS/SoDoKhachSan.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace PresentationLayer
+{
+    partial class SoDoKhachSan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SoDoKhachSan));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnRefesh = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHienTrangVsDatPhong = new System.Windows.Forms.Button();
+            this.btnQuaHan = new System.Windows.Forms.Button();
+            this.btnTongSoPhong = new System.Windows.Forms.Button();
+            this.btnPhongDangDon = new System.Windows.Forms.Button();
+            this.btnPhongChoKhach = new System.Windows.Forms.Button();
+            this.btnPhongDangO = new System.Windows.Forms.Button();
+            this.btnPhongTrong = new System.Windows.Forms.Button();
+            this.flowFloor = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowBody = new System.Windows.Forms.FlowLayoutPanel();
+            this.timerAutoChange = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(30)))), ((int)(((byte)(163)))));
+            this.panel2.Controls.Add(this.btnRefesh);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(835, 40);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnRefesh
+            // 
+            this.btnRefesh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnRefesh.FlatAppearance.BorderSize = 0;
+            this.btnRefesh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefesh.Font = new System.Drawing.Font("Noto Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefesh.ForeColor = System.Drawing.Color.White;
+            this.btnRefesh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefesh.Image")));
+            this.btnRefesh.Location = new System.Drawing.Point(178, 6);
+            this.btnRefesh.Name = "btnRefesh";
+            this.btnRefesh.Size = new System.Drawing.Size(23, 24);
+            this.btnRefesh.TabIndex = 2;
+            this.btnRefesh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnRefesh.UseVisualStyleBackColor = true;
+            this.btnRefesh.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnRefesh_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Noto Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(11, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 24);
+            this.button1.TabIndex = 1;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sơ Đồ Khách Sạn";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnHienTrangVsDatPhong);
+            this.panel1.Controls.Add(this.btnQuaHan);
+            this.panel1.Controls.Add(this.btnTongSoPhong);
+            this.panel1.Controls.Add(this.btnPhongDangDon);
+            this.panel1.Controls.Add(this.btnPhongChoKhach);
+            this.panel1.Controls.Add(this.btnPhongDangO);
+            this.panel1.Controls.Add(this.btnPhongTrong);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(835, 46);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnHienTrangVsDatPhong
+            // 
+            this.btnHienTrangVsDatPhong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(179)))), ((int)(((byte)(66)))));
+            this.btnHienTrangVsDatPhong.FlatAppearance.BorderSize = 0;
+            this.btnHienTrangVsDatPhong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHienTrangVsDatPhong.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHienTrangVsDatPhong.ForeColor = System.Drawing.Color.White;
+            this.btnHienTrangVsDatPhong.Image = ((System.Drawing.Image)(resources.GetObject("btnHienTrangVsDatPhong.Image")));
+            this.btnHienTrangVsDatPhong.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHienTrangVsDatPhong.Location = new System.Drawing.Point(629, 6);
+            this.btnHienTrangVsDatPhong.Name = "btnHienTrangVsDatPhong";
+            this.btnHienTrangVsDatPhong.Size = new System.Drawing.Size(182, 31);
+            this.btnHienTrangVsDatPhong.TabIndex = 7;
+            this.btnHienTrangVsDatPhong.Text = "      Hiện Trạng vs Đặt Phòng";
+            this.btnHienTrangVsDatPhong.UseVisualStyleBackColor = false;
+            this.btnHienTrangVsDatPhong.Click += new System.EventHandler(this.btnHienTrangVsDatPhong_Click);
+            // 
+            // btnQuaHan
+            // 
+            this.btnQuaHan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(85)))), ((int)(((byte)(72)))));
+            this.btnQuaHan.FlatAppearance.BorderSize = 0;
+            this.btnQuaHan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuaHan.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuaHan.ForeColor = System.Drawing.Color.White;
+            this.btnQuaHan.Location = new System.Drawing.Point(390, 7);
+            this.btnQuaHan.Name = "btnQuaHan";
+            this.btnQuaHan.Size = new System.Drawing.Size(100, 31);
+            this.btnQuaHan.TabIndex = 6;
+            this.btnQuaHan.Text = "03 Quá hạn";
+            this.btnQuaHan.UseVisualStyleBackColor = false;
+            // 
+            // btnTongSoPhong
+            // 
+            this.btnTongSoPhong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(36)))), ((int)(((byte)(170)))));
+            this.btnTongSoPhong.FlatAppearance.BorderSize = 0;
+            this.btnTongSoPhong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTongSoPhong.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTongSoPhong.ForeColor = System.Drawing.Color.White;
+            this.btnTongSoPhong.Location = new System.Drawing.Point(496, 6);
+            this.btnTongSoPhong.Name = "btnTongSoPhong";
+            this.btnTongSoPhong.Size = new System.Drawing.Size(117, 31);
+            this.btnTongSoPhong.TabIndex = 5;
+            this.btnTongSoPhong.Text = "Tổng phòng: 40";
+            this.btnTongSoPhong.UseVisualStyleBackColor = false;
+            // 
+            // btnPhongDangDon
+            // 
+            this.btnPhongDangDon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(136)))), ((int)(((byte)(229)))));
+            this.btnPhongDangDon.FlatAppearance.BorderSize = 0;
+            this.btnPhongDangDon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhongDangDon.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhongDangDon.ForeColor = System.Drawing.Color.White;
+            this.btnPhongDangDon.Location = new System.Drawing.Point(284, 7);
+            this.btnPhongDangDon.Name = "btnPhongDangDon";
+            this.btnPhongDangDon.Size = new System.Drawing.Size(100, 31);
+            this.btnPhongDangDon.TabIndex = 3;
+            this.btnPhongDangDon.Text = "03 Đang dọn";
+            this.btnPhongDangDon.UseVisualStyleBackColor = false;
+            // 
+            // btnPhongChoKhach
+            // 
+            this.btnPhongChoKhach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(87)))), ((int)(((byte)(34)))));
+            this.btnPhongChoKhach.FlatAppearance.BorderSize = 0;
+            this.btnPhongChoKhach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhongChoKhach.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhongChoKhach.ForeColor = System.Drawing.Color.White;
+            this.btnPhongChoKhach.Location = new System.Drawing.Point(180, 7);
+            this.btnPhongChoKhach.Name = "btnPhongChoKhach";
+            this.btnPhongChoKhach.Size = new System.Drawing.Size(98, 31);
+            this.btnPhongChoKhach.TabIndex = 2;
+            this.btnPhongChoKhach.Text = "03 Chờ khách";
+            this.btnPhongChoKhach.UseVisualStyleBackColor = false;
+            // 
+            // btnPhongDangO
+            // 
+            this.btnPhongDangO.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(57)))), ((int)(((byte)(53)))));
+            this.btnPhongDangO.FlatAppearance.BorderSize = 0;
+            this.btnPhongDangO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhongDangO.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhongDangO.ForeColor = System.Drawing.Color.White;
+            this.btnPhongDangO.Location = new System.Drawing.Point(96, 7);
+            this.btnPhongDangO.Name = "btnPhongDangO";
+            this.btnPhongDangO.Size = new System.Drawing.Size(78, 31);
+            this.btnPhongDangO.TabIndex = 1;
+            this.btnPhongDangO.Text = "03 Đang ở";
+            this.btnPhongDangO.UseVisualStyleBackColor = false;
+            // 
+            // btnPhongTrong
+            // 
+            this.btnPhongTrong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(179)))), ((int)(((byte)(66)))));
+            this.btnPhongTrong.FlatAppearance.BorderSize = 0;
+            this.btnPhongTrong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhongTrong.Font = new System.Drawing.Font("Noto Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhongTrong.ForeColor = System.Drawing.Color.White;
+            this.btnPhongTrong.Location = new System.Drawing.Point(12, 7);
+            this.btnPhongTrong.Name = "btnPhongTrong";
+            this.btnPhongTrong.Size = new System.Drawing.Size(78, 31);
+            this.btnPhongTrong.TabIndex = 0;
+            this.btnPhongTrong.Text = "03 Trống";
+            this.btnPhongTrong.UseVisualStyleBackColor = false;
+            // 
+            // flowFloor
+            // 
+            this.flowFloor.BackColor = System.Drawing.Color.White;
+            this.flowFloor.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowFloor.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowFloor.Location = new System.Drawing.Point(0, 86);
+            this.flowFloor.Margin = new System.Windows.Forms.Padding(0, 0, 1, 0);
+            this.flowFloor.Name = "flowFloor";
+            this.flowFloor.Size = new System.Drawing.Size(60, 614);
+            this.flowFloor.TabIndex = 5;
+            // 
+            // flowBody
+            // 
+            this.flowBody.BackColor = System.Drawing.Color.White;
+            this.flowBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowBody.Location = new System.Drawing.Point(60, 86);
+            this.flowBody.Margin = new System.Windows.Forms.Padding(1, 0, 0, 0);
+            this.flowBody.Name = "flowBody";
+            this.flowBody.Size = new System.Drawing.Size(775, 614);
+            this.flowBody.TabIndex = 6;
+            // 
+            // timerAutoChange
+            // 
+            this.timerAutoChange.Interval = 1000;
+            this.timerAutoChange.Tick += new System.EventHandler(this.timerAutoChange_Tick);
+            // 
+            // SoDoKhachSan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 700);
+            this.Controls.Add(this.flowBody);
+            this.Controls.Add(this.flowFloor);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Noto Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SoDoKhachSan";
+            this.Text = "SoDoKhachSan";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTongSoPhong;
+        private System.Windows.Forms.Button btnPhongDangDon;
+        private System.Windows.Forms.Button btnPhongChoKhach;
+        private System.Windows.Forms.Button btnPhongDangO;
+        private System.Windows.Forms.Button btnPhongTrong;
+        private System.Windows.Forms.FlowLayoutPanel flowFloor;
+        private System.Windows.Forms.FlowLayoutPanel flowBody;
+        private System.Windows.Forms.Button btnRefesh;
+        private System.Windows.Forms.Button btnQuaHan;
+        private System.Windows.Forms.Timer timerAutoChange;
+        private System.Windows.Forms.Button btnHienTrangVsDatPhong;
+    }
+}
+
