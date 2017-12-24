6956612188a2a64242e40a65358d0f7b4cce075e
AzureAD\TruongTuanKiet <truongtuankiet25121994@gmail.com>
2017-12-24 22:19:43 +0700

    config and DataProvider

---
+++ SourceCode/DataAccesLayer/DataProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataAccesLayer
+{
+	class DataProvider
+	{
+		public bool Connect()
+		{
+			SqlConnection conn = new SqlConnection();
+			conn.ConnectionString = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+			try
+			{
+				conn.Open();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}
