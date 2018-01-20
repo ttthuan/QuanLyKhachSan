0538c6827fb0fab312e500b26b3691f66e540a59
ttthuan <ttthuan001@gmail.com>
2018-01-20 09:54:29 +0700

    update

---
+++ SourceCode/WebAPIService/Controllers/DatPhongController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataTranferObject;
+using BusinessLayer;
+
+namespace WebAPIService.Controllers
+{
+    public class DatPhongController : ApiController
+    {
+        [HttpPost]
+        [Route("api/datphongs")]
+        public HttpResponseMessage DatPhongAPI([FromBody]PhieuThuePhongDTO phieuThuePhongDTO)
+        {
+            if(phieuThuePhongDTO != null)
+            {
+                PhieuThuePhongBUS phieuThuePhongBUS = new PhieuThuePhongBUS();
+                try
+                {
+                    phieuThuePhongBUS.ThemPhieuThuePhong(phieuThuePhongDTO);
+                    return Request.CreateResponse(HttpStatusCode.OK, "success");
+                }
+                catch(Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.GatewayTimeout, "error");
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.GatewayTimeout, "error");
+        }
+    }
+}
